3e4cbed121e74f174fba8bf13984f2b92b0d822d
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-03-26 11:21:54 -0600

    Initial commits for in-class Activity (7.3.1)-2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import DisplayIdeas from "./components/DisplayIdeas";
+import IdeasForm from "./components/IdeasForm"
 
 function App() {
+  const [ideas, setIdeas] = useState([
+    "Movie about a guy who gets bit by a radioactive mole rat",
+    "Romcom about two developers over Zoom",
+    "Horror film where AWS goes down for 24 hours"
+  ]);
+
+  const addIdeas = (newIdeas) => {
+    setIdeas([...ideas,newIdeas])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Welcome, Super Awesome Movie Monster Think Tank!</h1>
+
+      <DisplayIdeas ideas={ideas} />
+      <h1>Submit your ideas down below</h1>
+      <IdeasForm addIdeas={addIdeas} />
+    </main>
   );
 }
 
